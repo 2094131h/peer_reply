cc4c3bb91a72dd1d8ed1a307a270772630601594
2023503Gulen <guleninsay@hotmail.co.uk>
2015-03-21 20:35:53 +0000

    Added MyRegistrationView

--- peer_reply_project/peer_reply_project/urls.py
+++ peer_reply_project/peer_reply_project/urls.py
@@ -1,10 +1,18 @@
 from django.conf.urls import patterns, include, url
 from django.contrib import admin
 from django.conf import settings
+from registration.backends.simple.views import RegistrationView
+
+
+# Create a new class that redirects the user to create profile, if successful at logging
+class MyRegistrationView(RegistrationView):
+    def get_success_url(selfself,request, user):
+        return '/peer_reply/add_profile'
 
 urlpatterns = patterns('',
     url(r'^admin/', include(admin.site.urls)),
     url(r'^peer_reply/', include('peer_reply.urls')),
+    url(r'^accounts/register/$', MyRegistrationView.as_view(), name='registration_register'),
     (r'^accounts/', include('registration.backends.simple.urls')),)
 
 if settings.DEBUG:
